849459233e877b465a7103fe76bb64c5bfa95ae4
Joe Mitzman <joemitz@gmail.com>
2021-12-08 14:54:58 -0800

    Improved readability of code

--- maker.js
+++ maker.js
@@ -2,11 +2,14 @@ import MidiWriter from 'midi-writer-js';
 import fs from 'fs';
 
 const make = (melody, octave = 2, maxOctave = 6, duration = 1, repeat = 1, tracks = []) => {
+
   if (octave === maxOctave) {
     let trackNum = 1;
+
     tracks.forEach(track => {
       let write = new MidiWriter.Writer(track);
       let buffer = Buffer.from(write.buildFile());
+
       fs.writeFile(`midi/track${trackNum}.mid`, buffer, err => {
         if (err) {
           console.error(err);
@@ -14,18 +17,23 @@ const make = (melody, octave = 2, maxOctave = 6, duration = 1, repeat = 1, track
         }
       });
       trackNum++;
-    })
+    });
+
   } else {
     let transposedNotes = melody.map(note => {
       return `${note}${octave}`;
     });
+
     let track = new MidiWriter.Track();
     let notes = new MidiWriter.NoteEvent({pitch: transposedNotes, duration, repeat, sequential: true});
+
     track.addEvent(notes);
     tracks.push(track);
+
     make(melody, octave + 1, maxOctave, duration * 2, repeat * 2, tracks);
   }
 };
-let noteString = 'D,F,Bb,A,G,F,A,E';
+
+let noteString = 'C,G,A,D';
 let melody = noteString.split(',');
-make(melody, 2, 7);
+make(melody);
